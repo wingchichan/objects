0a37965d20c1dba834ce80f4c2a71d6b6332109f
Wing Chi Chan <w.chi.chan@gmail.com>
2025-01-14 15:58:38 +0000

    object methods workshop

--- app.js
+++ app.js
@@ -55,3 +55,58 @@ blogPost.tags.forEach(function (tag) {
 for (const tag of blogPost.tags) {
   console.log(tag);
 }
+
+// Object Methods
+
+// const person2 = {
+//   name: "Bob",
+//   age: 30,
+//   favouriteColour: "red",
+//   sayHello: function () {
+//     console.log("Hello!");
+//   },
+// };
+
+// person2.sayHello();
+
+const person2 = {
+  name: "Bob",
+  age: 30,
+  favouriteColour: "red",
+  sayHello: function (name) {
+    console.log("Hello " + name);
+  },
+  getAge: function () {
+    return this.age;
+  },
+};
+
+const age = person2.getAge(); // need to create variable to store the age in getAge
+console.log(age);
+
+// console.log(person2.age);
+
+const book = {
+  title: "The Dream",
+  author: "unknown",
+  pages: 20,
+  advert: function () {
+    console.log(
+      `Mystery book found titled ${book.title}, by ${book.author} author, with only ${book.pages} pages!`
+    );
+  },
+};
+
+book.advert();
+
+const book2 = {
+  title: "The Dream",
+  author: "unknown",
+  pages: 20,
+  advert: function () {
+    return this.author;
+  },
+};
+
+const author = book2.advert();
+console.log(author);
